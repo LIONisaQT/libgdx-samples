7fa219768f59583af2ca78f11fea8273e00d072b
yu-stephen98 <syu36@usfca.edu>
2018-07-06 11:56:12 -0700

    box2d properly works to scale

--- core/src/com/missionbit/actors/Player.java
+++ core/src/com/missionbit/actors/Player.java
@@ -5,42 +5,72 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.missionbit.states.InGame;
+import com.missionbit.states.State;
 
 public class Player extends Actor {
     private Texture texture;
-    private Vector2 velocity;
-    private float moveSpeed;
+    private Vector2 force;
     private Animation anim;
     private boolean faceRight, needFlip;
     private Vector3 tap;                    // Holds tap position
-    private static final int GRAVITY = -15; // Gravity constant
     private final InGame game;              // Reference to the in-game state
+
+    private BodyDef playerDef;
+    public Body playerBody;
+    private PolygonShape rect;
+    private FixtureDef fixtureDef;
 
     public Player(int x, int y, InGame game) {
         setDebug(true);
         this.game = game;
         texture = new Texture("texture-regions/run_cycle.png");
-        moveSpeed = 150;
+        force = new Vector2(0.5f, 0);
         anim = new Animation(new TextureRegion(texture), 8, 1f, 3, 3);
-        velocity = new Vector2(0, 0);
-        setPosition(x, y);
-        setBounds(x, y, texture.getWidth() / 3, texture.getHeight() / 3);
+        setBounds(x, y, texture.getWidth() /  3, texture.getHeight() / 3);
         faceRight = true;
         tap = new Vector3();
+
+        //create player def and fixture
+        playerDef = new BodyDef();
+        playerDef.type = BodyDef.BodyType.DynamicBody;
+        playerDef.position.set(x, y);
+        setPosition(playerDef.position.x, playerDef.position.y);
+
+
+        playerBody = game.world.createBody(playerDef);
+        playerBody.setFixedRotation(true);
+
+        rect = new PolygonShape();
+        rect.set(new float[]{(getWidth() / 3) * State.PIXEL_TO_METER, 0, (getWidth() / 3 + getWidth() / 2 - 10) * State.PIXEL_TO_METER, 0,
+                (getWidth() / 3 + getWidth() / 2 - 10) * State.PIXEL_TO_METER, getHeight() * State.PIXEL_TO_METER,
+                (getWidth() / 3) * State.PIXEL_TO_METER, getHeight() * State.PIXEL_TO_METER});
+
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = rect;
+        fixtureDef.density = 0.9f;
+        fixtureDef.friction = 1f;
+
+        playerBody.createFixture(fixtureDef);
+
+        rect.dispose();
     }
 
     public void update(float dt) {
         if (Gdx.input.isTouched()) {
-            if (getTapPosition().x > getX() + anim.getFrame().getRegionWidth() / 2) {
+            if (getTapPosition().x > playerBody.getPosition().x + (anim.getFrame().getRegionWidth() / 2 * State.PIXEL_TO_METER)) {
                 if (!faceRight) { needFlip = true; } // Check if we need to flip the animation
                 faceRight = true;
-                velocity.set(moveSpeed, velocity.y);
+                playerBody.applyLinearImpulse(force, new Vector2(getX(), getY()), true);
             } else {
                 if (faceRight) { needFlip = true; } // Check if we need to flip the animation
                 faceRight = false;
-                velocity.set(-moveSpeed, velocity.y);
+                playerBody.applyLinearImpulse(-force.x, force.y, getX(), getY(), true);
             }
 
             // Flips animation frames once if we need to
@@ -51,13 +81,9 @@ public class Player extends Actor {
 
             anim.update(dt);
         } else {
-            velocity.set(0, velocity.y);
             anim.setFrame(0);
         }
-        velocity.add(0, GRAVITY);
-        velocity.scl(dt);
-        moveBy(velocity.x, velocity.y);
-        velocity.scl(1 / dt);
+        setPosition(playerBody.getPosition().x, playerBody.getPosition().y);
     }
 
     /*
@@ -67,7 +93,7 @@ public class Player extends Actor {
     screen coordinates to world space.
      */
     private Vector3 getTapPosition() {
-        tap.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+        tap.set(Gdx.input.getX() , Gdx.input.getY() , 0);
         return game.camera.unproject(tap);
     }
 
--- core/src/com/missionbit/states/InGame.java
+++ core/src/com/missionbit/states/InGame.java
@@ -23,14 +23,14 @@ public class InGame extends State {
     private Player player;
     private TiledMapRenderer tiledMapRenderer;
     private TiledMap tiledMap;
-    private World world;
+    public World world;
     private Box2DDebugRenderer debugRenderer;
 
-    private BodyDef playerDef;
-    private Body playerBody;
-    private PolygonShape rect;
-    private FixtureDef fixtureDef;
-    private Fixture fixture;
+//    private BodyDef playerDef;
+//    private Body playerBody;
+//    private PolygonShape rect;
+//    private FixtureDef fixtureDef;
+//    private Fixture fixture;
 
     private BodyDef floorDef;
     PolygonShape floorShape;
@@ -39,36 +39,38 @@ public class InGame extends State {
 
     InGame(LibGDXSamples game) {
         super(game);
-        player = new Player(50, 100, this);
-        tiledMap = new TmxMapLoader().load("maps/TiledDemo.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        floors = new Array<Body>();
 
         //initialize box2d and world
         Box2D.init();
-        world = new World(new Vector2(0, -15), true);
+        world = new World(new Vector2(0, -9.8f), true);
         debugRenderer = new Box2DDebugRenderer();
 
-        //create player def and fixture
-        playerDef = new BodyDef();
-        playerDef.type = BodyDef.BodyType.DynamicBody;
-        playerDef.position.set(player.getX(), player.getY() + 50);
+        player = new Player(1, 4, this);
+        tiledMap = new TmxMapLoader().load("maps/TiledDemo.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, State.PIXEL_TO_METER);
 
+        floors = new Array<Body>();
 
-        playerBody = world.createBody(playerDef);
-
-        rect = new PolygonShape();
-        rect.set(new float[]{player.getWidth() / 3, 0, player.getWidth() / 3 + player.getWidth() / 2 - 10, 0,
-                player.getWidth() / 3 + player.getWidth() / 2 - 10, player.getHeight(), player.getWidth() / 3, player.getHeight()});
-
-        fixtureDef = new FixtureDef();
-        fixtureDef.shape = rect;
-        fixtureDef.density = 0.5f;
-        fixtureDef.friction = 0.4f;
-
-        fixture = playerBody.createFixture(fixtureDef);
-
-        rect.dispose();
+//        //create player def and fixture
+//        playerDef = new BodyDef();
+//        playerDef.type = BodyDef.BodyType.DynamicBody;
+//        playerDef.position.set(player.getX(), player.getY() + 50);
+//
+//
+//        playerBody = world.createBody(playerDef);
+//
+//        rect = new PolygonShape();
+//        rect.set(new float[]{player.getWidth() / 3, 0, player.getWidth() / 3 + player.getWidth() / 2 - 10, 0,
+//                player.getWidth() / 3 + player.getWidth() / 2 - 10, player.getHeight(), player.getWidth() / 3, player.getHeight()});
+//
+//        fixtureDef = new FixtureDef();
+//        fixtureDef.shape = rect;
+//        fixtureDef.density = 0.5f;
+//        fixtureDef.friction = 0.4f;
+//
+//        fixture = playerBody.createFixture(fixtureDef);
+//
+//        rect.dispose();
 
 
         //create floor def and fixture
@@ -76,10 +78,13 @@ public class InGame extends State {
         floorShape = new PolygonShape();
 
         for (PolygonMapObject obj : tiledMap.getLayers().get("Collision").getObjects().getByType(PolygonMapObject.class)) {
-            floorDef.position.set(obj.getPolygon().getX(), obj.getPolygon().getY());
+            floorDef.position.set(obj.getPolygon().getX() * State.PIXEL_TO_METER, obj.getPolygon().getY() * State.PIXEL_TO_METER);
             floors.add(world.createBody(floorDef));
-
-            floorShape.set(obj.getPolygon().getVertices());
+            float[] vertices = obj.getPolygon().getVertices();
+            for (int i = 0; i < vertices.length; i++) {
+                vertices[i] = vertices[i] * State.PIXEL_TO_METER;
+            }
+            floorShape.set(vertices);
             floors.get(counter).createFixture(floorShape, 0.0f);
             counter++;
         }
@@ -93,17 +98,17 @@ public class InGame extends State {
             game.setScreen(new MainMenu(game));
             dispose();
         }
-
-        player.setPosition(playerBody.getPosition().x, playerBody.getPosition().y);
     }
 
     @Override
     void drawGame() {
+        game.batch.setProjectionMatrix(camera.combined);
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
         game.batch.begin();
-        game.font.draw(game.batch, this.getClass().toString(), 0, LibGDXSamples.HEIGHT);
-        game.batch.draw(player.getTexture(), player.getX(), player.getY());
+//        game.font.draw(game.batch, this.getClass().toString(), 0, LibGDXSamples.HEIGHT * State.PIXEL_TO_METER);
+        game.batch.draw(player.getTexture(), player.getX(), player.getY(),
+                player.getTexture().getRegionWidth() * State.PIXEL_TO_METER, player.getTexture().getRegionHeight() * State.PIXEL_TO_METER);
         game.batch.end();
 
         debugRenderer.render(world, camera.combined);
